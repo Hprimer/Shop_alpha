94ed878f20bbf89ceb91ed5bbf8f532edc64662f
Hprimer <04200620z@gmail.com>
2025-03-21 14:24:55 +0400

    add product page

--- src/Components/Item/Item.tsx
+++ src/Components/Item/Item.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BiSolidLike } from "react-icons/bi";
 
 import './item.css'
+import { Link } from 'react-router-dom';
 
 interface Product {
   id: number;
@@ -19,17 +21,37 @@ interface ItemProps {
 const Item: React.FC<ItemProps> = ({item, onAdd}) =>{	
 // function Item({item, onAdd}) {
 	// const imgPath = require(`./img/${props.item.img}`).default;
+	const [isLiked, setIsLiked] = useState(false);
+
+	const handleLike = () => {
+	  setIsLiked(!isLiked);
+	};
 
 	return (
-	
-			<div className='item col-12 col-md-4 col-lg-3 '>
+		
+		<div className='item col-12 col-md-4 col-lg-3 '>
+			<Link to='/Product/'  className='item_link'>
 				{/* <img src={process.env.PUBLIC_URL + "/imgs/" + props.item.img}></img>*/} {/*чтобы из public папки брать фото */}
 				<img src={item.image_path} alt={item.name}/>
 				<h2 className='item-h2'> {item.name} </h2>
 				<p>{item.wood_type}, {item.finish} finish</p>
+				{/* <div className='block_item'>
+					<b>{item.price}$</b>
+					<div className='like'><BiSolidLike/></div>
+				</div> */}
 				<b>{item.price}$</b>
-				<div className='btn_add' onClick={() => onAdd(item)}> + </div>
-			</div>
+				<div className={`like ${isLiked ? 'active' : ''}`} 
+					onClick={(e) => {
+					e.stopPropagation(); // Блокирует переход
+					handleLike();
+					}}
+				><BiSolidLike/>
+				</div>
+				{/* <div className='btn_add' onClick={() => onAdd(item)}> + </div> */}
+				{/* <div className='like'><BiSolidLike/></div> */}
+			</Link>
+			
+		</div>
 
 	)
 }
---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "Product.css"
+
+function Product() {
+  return (
+    <div className='container product_container'>
+        Product
+    </div>
+
+  )
+}
+
+export default Product
